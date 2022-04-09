6d720ddc5b69f5b7f075bdf19064cd92add18fb7
sjstanev <102998517+sjstanev@users.noreply.github.com>
2022-04-09 14:05:53 +0300

    Update 02. Ad Astra.py

--- python-fund/fundamentals - exams/02. Ad Astra.py
+++ python-fund/fundamentals - exams/02. Ad Astra.py
@@ -1,5 +1,7 @@
 import re
 
+# #Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|
+
 pattern = r'(#|\|)(?P<food>[A-Za-z\s]+)(\1)(?P<day>\d{2}/\d{2}/\d{2})(\1)(?P<calories>[1-9][0-9]{1,3}|10000)(\1)'
 text = input()
 calories = 0
