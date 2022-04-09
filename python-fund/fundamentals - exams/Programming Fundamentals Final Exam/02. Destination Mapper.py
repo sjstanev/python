d02c16c3cda750efcb2b59e7010e85e42d98b815
sjstanev <sstanev@vtechnology.eu>
2022-04-09 23:01:44 +0300

    Debug

--- python-fund/fundamentals - exams/Programming Fundamentals Final Exam/02. Destination Mapper.py
+++ python-fund/fundamentals - exams/Programming Fundamentals Final Exam/02. Destination Mapper.py
@@ -1,1 +1,15 @@
+import re
 
+text = input()
+
+pattern = r'(=|/)(?P<town>[A-Z][A-za-z]{2,})(\1)'
+matches = re.finditer(pattern,text)
+
+travel_points = 0
+town = []
+for match in matches:
+    travel_points += len(match.group('town'))
+    town.append(match.group('town'))
+
+print(f"Destinations: {', '.join(town)}")
+print(f'Travel Points: {travel_points}')
