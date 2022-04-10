f344b202774879c89df742a20e2d90155fcedc2d
sjstanev <sstanev@vtechnology.eu>
2022-04-10 13:22:14 +0300

    source code

--- python-fund/fundamentals - exams/Exam Preparation part 1/01. Activation Keys.py
+++ python-fund/fundamentals - exams/Exam Preparation part 1/01. Activation Keys.py
@@ -1,26 +1,26 @@
 activation_key = input()
-commnad = input()
+command = input()
 
-while not commnad == "Generate":
-    commnad = commnad.split(">>>")
-    if commnad[0] == "Contains":
-        if commnad[1] in activation_key:
-            print(f"{activation_key} contains {commnad[1]}")
+while not command == "Generate":
+    command = command.split(">>>")
+    if command[0] == "Contains":
+        if command[1] in activation_key:
+            print(f"{activation_key} contains {command[1]}")
         else:
             print("Substring not found!")
-    if commnad[0] == "Flip":
-        startIndex = int(commnad[2])
-        endIndex = int(commnad[3])
-        if commnad[1] == "Upper":
+    if command[0] == "Flip":
+        startIndex = int(command[2])
+        endIndex = int(command[3])
+        if command[1] == "Upper":
             activation_key = activation_key[:startIndex] + activation_key[startIndex:endIndex].upper() + activation_key[endIndex:]
-        elif commnad[1] == "Lower":
+        elif command[1] == "Lower":
             activation_key = activation_key[:startIndex] + activation_key[startIndex:endIndex].lower() + activation_key[endIndex:]
         print(activation_key)
-    if commnad[0] == "Slice":
-        startIndex = int(commnad[1])
-        endIndex = int(commnad[2])
+    if command[0] == "Slice":
+        startIndex = int(command[1])
+        endIndex = int(command[2])
         activation_key = activation_key[:startIndex] + activation_key[endIndex:]
         print(activation_key)
-    commnad = input()
+    command = input()
 
 print(f"Your activation key is: {activation_key}")
