9090840e521ebe2d96d6b5ae611da81887f9b0dc
stanev raven <sstanev@vtechnology.eu>
2023-11-27 16:03:55 +0200

    stack exercise

---
+++ Python Advanced  26102023/4. Exercise - List as Stacks and Queues/reverse_numbers.py
@@ -0,0 +1,23 @@
+"""
+Input:
+1 2 3 4 5
+
+Output:
+5 4 3 2 1
+"""
+
+stack = input().split()
+reverse_stack = []
+
+while stack:
+    reverse_stack.append(stack.pop())
+
+print(' '.join(reverse_stack))
+
+"""
+BETTER SOLUTION
+
+while stack:
+    last_number = stack.pop()
+    print(last_number, end = ' ')
+"""
---
+++ Python Advanced  26102023/4. Exercise - List as Stacks and Queues/truck_tour.py
@@ -0,0 +1,30 @@
+"""
+Input
+3
+1 5
+10 3
+3 4
+"""
+from collections import deque
+
+stations = int(input())
+# amount_of_petrol, distance = int(input().split())
+pumps = deque()
+
+for _ in range(stations):
+    pumps.append([int(x) for x in input().split()])
+
+for attempt in range(stations):
+    trunk = 0
+    failed = False
+    for petrol, distance in pumps:  # we do unpacking
+        trunk = trunk + petrol - distance
+        if trunk < 0:
+            failed = True
+            break
+
+    if failed:
+        pumps.append(pumps.popleft())
+    else:
+        print(attempt)
+        break
