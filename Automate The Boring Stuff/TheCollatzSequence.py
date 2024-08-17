0e29c480e11d7813a2cd7382985b8e43bf79c3b7
stanev raven <sstanev@vtechnology.eu>
2024-08-17 23:54:04 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ Automate The Boring Stuff/TheCollatzSequence.py
@@ -0,0 +1,22 @@
+def collatz(number):
+
+    if number % 2 == 0:
+        print('number // 2')
+        return number // 2
+    else:
+        print('3 * number + 1')
+        return 3 * number + 1
+
+while True:
+    print('Input your number')
+    user_number = input()
+    try:
+        user_number = int(user_number)
+
+        collatz_number = collatz(user_number)
+        print(collatz_number)
+
+        if collatz_number == 1:
+            break
+    except:
+        print('must enter an integer')
